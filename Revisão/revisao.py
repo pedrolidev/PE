54182bc953ffc25b12d08eb656e57b1fce3e706c
Pedro Oliveira <126485301+ppedr0liveira@users.noreply.github.com>
2023-09-19 15:11:35 -0300

    Add files via upload

---
+++ Revisão/revisao.py
@@ -0,0 +1,26 @@
+def valia_dados(texto):
+    """Retorna True caso texto seja numérico, e False caso contrário."""
+    pass
+
+def gera_numeros(num_dados, tamanho_dado):
+    """Retorna uma sequência de números gerados aleatoriamente."""
+    pass
+
+def leitura_dados():
+    """Lê os dados, e continua pedindo para o usuário enquanto o dado passado
+    não for válido. Retorna o valor já convertido.
+    """
+    # Ler os valores
+    # Validar os dado
+    # Se o dado for válido, converte para int
+    # retun dados_validados
+    pass
+
+
+
+def main():
+    # ler os valores válidos
+    num_dados = leitura_dados()
+    # Gerar os números
+    # Encerrar o programa
+    pass
